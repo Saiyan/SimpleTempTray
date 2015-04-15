8856d063cae1854bb2407fd48eab04adabe466b0
Saiyan <saiyan@cssai.eu>
2015-04-15 13:50:44 +0200

    changed default value of HideOnStart to false

--- SimpleTempTray/Properties/Settings.Designer.cs
+++ SimpleTempTray/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace SimpleTempTray.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool HideOnStart {
             get {
                 return ((bool)(this["HideOnStart"]));
